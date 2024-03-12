ae2958b9980816ceef569de64adee9e0a296f0e1
Piyush Kumar Gupta <109588990+piyush21g@users.noreply.github.com>
2024-03-12 12:31:06 +0000

    done with products page

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,24 +1,209 @@
-import React from 'react';
-import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
+import React, { useState } from "react";
+import {
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  TextField,
+} from "@mui/material";
 
-import { kanbanData, kanbanGrid } from '../data/dummy';
-import { Header } from '../components';
 
-const Kanban = () => (
-  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-    <Header category="App" title="Kanban" />
-    <KanbanComponent
-      id="kanban"
-      keyField="Status"
-      dataSource={kanbanData}
-      cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
-    >
-      <ColumnsDirective>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
-      </ColumnsDirective>
-    </KanbanComponent>
-  </div>
-);
+const Kanban = () => {
+
+
+  const [products, setProducts] = useState([
+    { id: 1, name: "Potato", price: 2.99, quantity: 250 },
+    { id: 2, name: "Onion", price: 3.99, quantity: 230 },
+    { id: 3, name: "Tomato", price: 5.99, quantity: 225 },
+    { id: 4, name: "Burger", price: 1.99, quantity: 225 },
+  ]);
+
+  const [isAddDialogOpen, setAddDialogOpen] = useState(false);
+  const [isEditDialogOpen, setEditDialogOpen] = useState(false);
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [newProduct, setNewProduct] = useState({ name: "", price: "", quantity: "" });
+
+  const handleAddProduct = () => {
+    setProducts([...products, { ...newProduct, id: products.length + 1 }]);
+    setAddDialogOpen(false);
+    setNewProduct({ name: "", price: 0, quantity: 0 });
+  };
+
+  const handleEditProduct = () => {
+    const updatedProducts = products.map((product) =>
+      product.id === selectedProduct.id ? { ...product, ...newProduct } : product
+    );
+    setProducts(updatedProducts);
+    setEditDialogOpen(false);
+    setSelectedProduct(null);
+    setNewProduct({ name: "", price: 0, quantity: 0 });
+  };
+
+  const handleDeleteProduct = (id) => {
+    const updatedProducts = products.filter((product) => product.id !== id);
+    setProducts(updatedProducts);
+  };
+
+  const handleEditClick = (product) => {
+    setSelectedProduct(product);
+    setNewProduct(product);
+    setEditDialogOpen(true);
+  };
+
+  return (
+    <Box m="20px">
+  
+      <Button
+        style={{ backgroundColor: "aqua", marginBottom: 10 + "px" }}
+        onClick={() => setAddDialogOpen(true)}
+      >
+        Add Product
+      </Button>
+
+      <TableContainer style={{ background: "#143368" }} component={Paper}>
+        <Table>
+          <TableHead style={{ background: "#a4a9fc" }}>
+            <TableRow >
+              <TableCell align="center">S. No.</TableCell>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Price</TableCell>
+              <TableCell align="center">Quantity</TableCell>
+              <TableCell align="center">Actions</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {products.map((product, index) => (
+              <TableRow key={product.id} style={{ background: "rgb(242 240 240)" }}>
+                <TableCell align="center">{index + 1}</TableCell>
+                <TableCell align="center">{product.name}</TableCell>
+                <TableCell align="center">${product.price.toFixed(2)}</TableCell>
+                <TableCell align="center">{product.quantity}</TableCell>
+                <TableCell align="center">
+                  <Button
+                    style={{ backgroundColor: "aqua", marginBottom: 10 + "px", marginRight:4+"px"}}
+                    onClick={() => handleEditClick(product)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    style={{ backgroundColor: "aqua", marginBottom: 10 + "px" }}
+                    onClick={() => handleDeleteProduct(product.id)}
+                  >
+                    Delete
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      {/* Add Product Dialog */}
+      <Dialog open={isAddDialogOpen} onClose={() => setAddDialogOpen(false)}>
+        <DialogTitle style={{ background: "#a4a9fc" }}>Add Product</DialogTitle>
+        <DialogContent style={{ background: "rgb(242 240 240)" }}>
+          <TextField
+            label="Name"
+            value={newProduct.name}
+            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Price"
+            type="number"
+            value={newProduct.price}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, price: parseFloat(e.target.value) })
+            }
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Quantity"
+            type="number"
+            value={newProduct.quantity}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, quantity: parseInt(e.target.value) })
+            }
+            fullWidth
+            margin="normal"
+          />
+        </DialogContent>
+        <DialogActions style={{ background: "#a4a9fc" }}>
+          <Button
+            style={{ backgroundColor: "aqua", marginBottom: 10 + "px" }}
+            onClick={() => setAddDialogOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            style={{ backgroundColor: "aqua", marginBottom: 10 + "px" }}
+            onClick={handleAddProduct}
+          >
+            Add
+          </Button>
+        </DialogActions>
+      </Dialog>
+
+      {/* Edit Product Dialog */}
+      <Dialog open={isEditDialogOpen} onClose={() => setEditDialogOpen(false)}>
+        <DialogTitle style={{ background: "rgb(14, 29, 56)" }}>Edit Product</DialogTitle>
+        <DialogContent style={{ background: "#143368" }}>
+          <TextField
+            label="Name"
+            value={newProduct.name}
+            onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Price"
+            type="number"
+            value={newProduct.price}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, price: parseFloat(e.target.value) })
+            }
+            fullWidth
+            margin="normal"
+          />
+          <TextField
+            label="Quantity"
+            type="number"
+            value={newProduct.quantity}
+            onChange={(e) =>
+              setNewProduct({ ...newProduct, quantity: parseInt(e.target.value) })
+            }
+            fullWidth
+            margin="normal"
+          />
+        </DialogContent>
+        <DialogActions style={{ background: "rgb(14, 29, 56)" }}>
+          <Button
+            style={{ backgroundColor: "aqua", marginBottom: 10 + "px" }}
+            onClick={() => setEditDialogOpen(false)}
+          >
+            Cancel
+          </Button>
+          <Button
+            style={{ backgroundColor: "aqua", marginBottom: 10 + "px" }}
+            onClick={handleEditProduct}
+          >
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Box>
+  );
+};
 
 export default Kanban;
